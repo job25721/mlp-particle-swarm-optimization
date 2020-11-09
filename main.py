0fee15b35e2fd60d38a19bc1ed91321faf112325
job25721 <pathomporn_pankaew@cmu.ac.th>
2020-11-10 02:05:58 +0700

    initail commit

---
+++ main.py
@@ -0,0 +1,152 @@
+import time
+import csv
+import numpy as np
+from swarm import Model, Node
+from functions import printProgressBar, cross_validation_split, select_validate
+import matplotlib.pyplot as plt
+
+
+def preprocessData():
+    file = open('AirQualityUCI.csv', 'r')
+    reader = csv.reader(file)
+    data = []
+    preData = {
+        "3": [],
+        "6": [],
+        "8": [],
+        "10": [],
+        "11": [],
+        "12": [],
+        "13": [],
+        "14": [],
+        "desire_output": []
+    }
+    inputIdx = [3, 6, 8, 10, 11, 12, 13, 14]
+    for i, row in enumerate(reader):
+        if i > 0:
+            preData['desire_output'].append(float(row[5]))
+            for idx in inputIdx:
+                preData[f'{idx}'].append(float(row[idx]))
+    file.seek(0)
+    maxOutput = np.max(preData['desire_output'])
+    meanOutput = np.average(preData['desire_output'])
+    inputVal = {
+        "3": {"max": 0, "mean": 0},
+        "6": {"max": 0, "mean": 0},
+        "8": {"max": 0, "mean": 0},
+        "10": {"max": 0, "mean": 0},
+        "11": {"max": 0, "mean": 0},
+        "12": {"max": 0, "mean": 0},
+        "13": {"max": 0, "mean": 0},
+        "14": {"max": 0, "mean": 0},
+    }
+    for idx in inputIdx:
+        inputVal[f'{idx}']['max'] = np.max(preData[f'{idx}'])
+        inputVal[f'{idx}']['mean'] = np.average(preData[f'{idx}'])
+    for i, row in enumerate(reader):
+        printProgressBar(i, 9358, 'preproceeing...', '', length=50)
+        if i > 0:
+            if float(row[5]) == -200:
+                row[5] = meanOutput
+            dic = {
+                "date": row[0],
+                "time": row[1],
+                "input": [],
+                "desire_output": float(row[5]) / maxOutput
+            }
+
+            for idx in inputIdx:
+                if float(row[idx]) == -200:
+                    row[idx] = inputVal[f'{idx}']['mean']
+                dic['input'].append(
+                    float(row[idx]) / inputVal[f'{idx}']['max'])
+
+            data.append(dic)
+    return data
+
+
+def buildLayer(node):
+    return [Node() for i in range(node)]
+
+
+data = preprocessData()
+
+particle_n = 10
+population = []
+
+print("building..")
+for p in range(particle_n):
+    inputLayer = buildLayer(node=8)
+    hiddenLayers = [buildLayer(node=np.random.randint(3, 6))
+                    for i in range(np.random.randint(1, 3))]
+    outputLayer = buildLayer(node=1)
+    print(f'p : {p}', end=" ")
+    particle = Model()
+    particle.create(inputLayer, hiddenLayers, outputLayer)
+    print(
+        f'shape :{len(particle.inputLayer)} - {[len(l) for l in particle.hiddenLayers]} - {len(particle.outputLayer)}')
+    population.append(particle)
+
+
+time.sleep(3)
+
+
+def velocity_vector(xpbest, xi, vi):
+    return vi + (np.random.uniform(0, 1)*(xpbest - xi))
+
+
+print('training..')
+t_max = 25
+
+cross_data = cross_validation_split(cross_validate_num=0.1, dataset=data)
+block = cross_data["data_block"]
+rand_set = cross_data["rand_set"]
+remiander_set = cross_data["rem_set"]
+
+cross_validation_plot = []
+for c in range(10):
+    res = select_validate(block, rand_set, c, remiander_set)
+    train = res["train"]
+    cross_valid = res["cross_valid"]
+    wins = []
+    printProgressBar(0, t_max, prefix='evolutioning',
+                     suffix='', length=25)
+    tic = time.perf_counter()
+    for t in range(t_max):
+        for i, p in enumerate(population):
+            if t > 0:
+                p.updateNeuralNetwork()
+            fx = p.evaluate(train)  # evaluate performance
+            if fx < p.pbest:
+                p.pbest = fx
+                p.xpbest = p.x
+
+            if t == t_max - 1:
+                wins.append(p.pbest)
+
+            # velocity calc
+            v_new = velocity_vector(xpbest=p.xpbest, xi=p.x, vi=p.v)
+            p.v = v_new
+            # update position
+            p.x = p.x + v_new
+
+        printProgressBar(
+            t+1, t_max, prefix=f'evolutioning.. t={t+1} ,cross_validation : {c+1}', length=25)
+    # cross validation
+    best_validation = []
+    for p in population:
+        mae = p.evaluate(cross_valid)
+        best_validation.append(mae)
+    cross_validation_plot.append(np.min(best_validation))
+
+    toc = time.perf_counter()
+    print(f'pbest in all gen = {np.min(wins)}')
+    if toc-tic >= 60:
+        print(f"total execution time {((toc - tic)/60):0.2f} m\n")
+    else:
+        print(f"total execution time {toc - tic:0.4f} secs\n")
+
+
+plt.plot(cross_validation_plot)
+plt.title('cross validation')
+plt.show()
---
+++ swarm.py
@@ -0,0 +1,105 @@
+from functions import printProgressBar
+import numpy as np
+
+
+def sigmoid(x):
+    return 1 / (1 + np.e**(-x))
+
+
+class Node:
+    def __init__(self):
+        self.w = []
+        self.b = 0.0
+        self.input = []
+        self.y = 0
+
+    def initialWeight(self, n):
+        self.w = np.random.uniform(-0.5, 0.5, size=n).reshape(n, 1)
+        # self.b = np.random.uniform(-1, 1)
+
+    def addInput(self, inputVector):
+        self.input = inputVector
+
+    def calculate_output(self, net):
+        if len(net) != 0 and len(self.w) != 0:
+            self.y = sigmoid(np.array(net).dot(self.w)[0] + self.b)
+        else:
+            self.y = np.array(self.input)
+
+
+class Model:
+    def __init__(self):
+        self.particle = []
+        self.x = []
+        self.pbest = float('inf')
+        self.xpbest = []
+        self.v = []
+
+    def feed_forward(self, inputVector):
+        for l, layer in enumerate(self.particle):
+            for i, node in enumerate(layer):
+                if l == 0:
+                    node.addInput(inputVector[i])
+                    node.calculate_output([])
+                else:
+                    net = [prev.y for prev in self.particle[l-1]]
+                    node.calculate_output(net)
+
+    def create(self, inputLayer, hiddenLayers, outputLayer):
+        self.inputLayer = inputLayer
+        self.hiddenLayers = hiddenLayers
+        self.outputLayer = outputLayer
+
+        self.particle.append(self.inputLayer)
+
+        for layer in self.hiddenLayers:
+            self.particle.append(layer)
+        self.particle.append(self.outputLayer)
+
+        for node in self.outputLayer:
+            node.initialWeight(
+                n=len(self.hiddenLayers[len(self.hiddenLayers) - 1]))
+            for w in node.w:
+                self.x.append(w)
+
+        for l, layer in enumerate(self.hiddenLayers):
+            for node in layer:
+                if l == 0:
+                    n = len(self.inputLayer)
+                else:
+                    n = len(self.hiddenLayers[l-1])
+                node.initialWeight(n)
+                for w in node.w:
+                    self.x.append(w)
+
+        self.x = np.array(self.x)
+        self.v = np.array([np.random.uniform(-0.5, 0.5)
+                           for i in range(len(self.x))]).reshape(len(self.x), 1)
+
+    def updateNeuralNetwork(self):
+        count = 0
+        for node in self.outputLayer:
+            for i in range(len(node.w)):
+                node.w[i] = self.x[count]
+                count = count+1
+        for layer in self.hiddenLayers:
+            for node in layer:
+                for i in range(len(node.w)):
+                    node.w[i] = self.x[count]
+                    count = count+1
+
+    def evaluate(self, data):
+        predict = 120
+        MAE = []
+        for n, train in enumerate(data):
+            if n + predict <= len(data) - 1:
+                # print(n)
+                # print(train['date'], train['time'])
+                # print(data[n+predict]['date'], data[n+predict]['time'])
+                # print()
+                self.feed_forward(train['input'])
+                for node in self.outputLayer:
+                    MAE.append(
+                        np.abs(node.y - data[n+predict]['desire_output']))
+        mean_absolute_err = np.average(MAE)
+        return mean_absolute_err
